54c9de07e2b0aca5ddf75f824303140856bb8ad5
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-02-17 20:39:49 +0500

    added bulma, remove unused code

--- src/background/index.js
+++ src/background/index.js
@@ -2,13 +2,3 @@
 chrome.runtime.onInstalled.addListener(details => {
   console.log(details)
 })
-
-chrome.webRequest.onBeforeRequest.addListener(
-  (a, b, c, d, e) => {
-    console.log(a, b, c, d, e)
-  },
-  {
-    urls: ['http://*/*', 'https://*/*']
-  },
-  ['blocking', 'requestBody']
-)
