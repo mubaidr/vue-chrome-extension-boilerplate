52ee7d1a0011310b3e0a0eaf7c0ff8be9cd7673a
Muhammad Ubaid Raza <mubaidr@gmail.com>
2021-10-21 08:55:20 +0500

    Update index.js

--- src/background/index.js
+++ src/background/index.js
@@ -1,4 +1,38 @@
-// OnInstall handler
-chrome.runtime.onInstalled.addListener(details => {
-  console.log(details)
+let settings = {
+  isEnabled: true,
+}
+
+// dfault settings for development enviroment
+if (process.env.NODE_ENV === 'development') {
+  settings = {
+    isEnabled: true,
+  }
+
+  console.info('Extension initiliazed with settings: ', settings)
+}
+
+function loadSettings() {
+  chrome.storage.sync.get(items => {
+    if (items.settings) settings = items.settings
+  })
+}
+
+function saveSettings(updated) {
+  if (updated) settings = updated
+
+  chrome.storage.sync.set({
+    settings,
+  })
+}
+
+chrome.runtime.onMessage.addListener(message => {
+  if (!settings.isEnabled) return
+
+  console.log(message, settings)
 })
+
+chrome.runtime.onInstalled.addListener(() => {
+  saveSettings()
+})
+
+loadSettings()
